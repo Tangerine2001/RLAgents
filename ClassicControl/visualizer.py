45ce4406a3012a0ed376e627328eb713bd89155a
Tangerine2001 <maxtang2001@gmail.com>
2021-10-22 13:00:18 -0400

    added model and cartpoleAgent

---
+++ ClassicControl/visualizer.py
@@ -0,0 +1,31 @@
+import re
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+
+def main():
+    data = clean('CartPoleResultsv1.csv')
+    plot(data)
+    plt.show()
+
+
+def clean(inputPath: str) -> pd.DataFrame:
+    df = pd.read_csv(inputPath, index_col=0)
+    names = []
+    scores = []
+    for column in df.columns:
+        names += re.findall("([0-9].[0-9])", column) * 1000
+        scores += list(df[column])
+    new_df = pd.DataFrame()
+    new_df['Version'] = names
+    new_df['Scores'] = scores
+    return new_df
+
+
+def plot(data: pd.DataFrame):
+    sns.catplot(x='Scores', y='Version', data=data, palette=sns.color_palette('flare'))
+
+
+if __name__ == '__main__':
+    main()
